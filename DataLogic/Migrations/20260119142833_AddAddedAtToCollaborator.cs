2a13864c3fdb4a8dcf0f862ebdace1ecac87ae87
nvsingh2001 <nvsingh2001@hotmail.com>
2026-01-20 15:01:10 +0530

    Add: Update Collaborator Entity.

---
+++ DataLogic/Migrations/20260119142833_AddAddedAtToCollaborator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLogic.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAddedAtToCollaborator : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "AddedAt",
+                table: "Collaborators",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AddedAt",
+                table: "Collaborators");
+        }
+    }
+}
